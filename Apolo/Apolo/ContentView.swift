08d8e2747a99119f9e278fd65080fc79c44866f3
TemaHapkin <temahapkin@gmail.com>
2020-09-09 20:52:17 +0300

    commit_1

---
+++ Apolo/Apolo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apolo
+//
+//  Created by Артём Бутырин on 09.09.2020.
+//  Copyright © 2020 Артём Бутырин. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
